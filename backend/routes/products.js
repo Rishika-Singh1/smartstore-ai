a5d7abf522568d24edf728b5d524ba6eb97aeab4
Rishika Singh <55singhrishika@gmail.com>
2026-05-22 20:05:56 +0530

    Configure product management routes

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,8 +1,35 @@
 const express = require('express');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json({ message: 'Route working ✅' });
-});
+const {
+  getProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getProduct,
+} = require('../controllers/productController');
+
+const {
+  protect,
+} = require('../middleware/auth');
+
+// Protect all product routes
+router.use(protect);
+
+// Routes
+router.route('/')
+
+  .get(getProducts)
+
+  .post(createProduct);
+
+router.route('/:id')
+
+  .get(getProduct)
+
+  .put(updateProduct)
+
+  .delete(deleteProduct);
 
 module.exports = router;
